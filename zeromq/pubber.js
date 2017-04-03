5ab7cf3ff82bfffc2548d45f3f3b45e4b5c1d287
Chathura Wijesinghe <cdanasiri@gmail.com>
2017-04-03 15:11:09 +0530

    modified bind address for heroku testing

--- zeromq/pubber.js
+++ zeromq/pubber.js
@@ -2,11 +2,11 @@
 var zmq = require('zmq')
   , sock = zmq.socket('pub');
 
-sock.bind('tcp://0.0.0.0:5563');
-console.log('Publisher bound to port 8831');
+// sock.bind('tcp://0.0.0.0:5563');
+// console.log('Publisher bound to port 8831');
 
-setInterval(function(){
-  console.log('sending a multipart message envelope');
-  sock.send(['kitty cats', 'meow!']);
-}, 3000);
+// setInterval(function(){
+//   console.log('sending a multipart message envelope');
+//   sock.send(['kitty cats', 'meow!']);
+// }, 3000);
 
